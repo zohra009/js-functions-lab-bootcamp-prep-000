c3bb3807fd5bc562163ac771eafc096d55f44510
anzohra@outlook.com <anzohra@outlook.com>
2018-04-03 00:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "happyHolidays!"
+}
+function happyHolidaysTo() {
+  return "happyHolidays, ${name}!"
+}
+function happyHolidaysTo() {
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown() {
+  return "It's ${days} days until ${holiday}!"
+}
