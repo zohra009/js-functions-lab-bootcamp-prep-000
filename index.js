7e1da3f2d4d9608e4623734c9c1766ed16fe20ab
anzohra@outlook.com <anzohra@outlook.com>
2018-04-03 00:45:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function happyHolidays() {
   return "happyHolidays!"
 }
+function happyHolidaysTo() {
+  return "happyHolidays, ${name}!"
+}
+function happyHolidaysTo() {
+  return "Happy ${holiday}, ${name}!"
+}
+function holidayCountdown() {
+  return "It's ${days} days until ${holiday}!"
+}
